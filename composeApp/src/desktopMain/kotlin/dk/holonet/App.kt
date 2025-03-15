c9a802a0170963d26cacc543648e0a72b4eeaceb
Mathias Jelshøj <mathias.j@shapegames.com>
2025-03-15 12:44:07 +0100

    Optimize imports

--- composeApp/src/desktopMain/kotlin/dk/holonet/App.kt
+++ composeApp/src/desktopMain/kotlin/dk/holonet/App.kt
@@ -1,26 +1,11 @@
 package dk.holonet
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
 import dk.holonet.components.BorderPane
 import dk.holonet.theme.HoloNetTheme
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import holonet.composeapp.generated.resources.Res
-import holonet.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
