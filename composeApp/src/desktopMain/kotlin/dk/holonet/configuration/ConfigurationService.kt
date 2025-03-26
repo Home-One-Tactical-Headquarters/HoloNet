919272b8c6f29517c25433ee01563bff02ebe44c
Mathias Jelshøj <mathias.j@shapegames.com>
2025-03-26 14:08:59 +0100

    Load and save configuration to user.home

--- composeApp/src/desktopMain/kotlin/dk/holonet/configuration/ConfigurationService.kt
+++ composeApp/src/desktopMain/kotlin/dk/holonet/configuration/ConfigurationService.kt
@@ -4,6 +4,7 @@ import dk.holonet.core.HolonetConfiguration
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.*
+import java.io.File
 
 class ConfigurationService {
 
@@ -12,10 +13,33 @@ class ConfigurationService {
 
     suspend fun fetchConfiguration(): HolonetConfiguration {
         return withContext(Dispatchers.IO) {
-            val jsonString = testJson
+
+            // Load configuration if it exists in user.home
+            val file = File(System.getProperty("user.home") + "/holonet/config.json")
+            val configExists = file.exists() && file.isFile
+
+            // If configuration exists, read it. Otherwise, use default configuration
+            val jsonString: String = if (configExists) file.readText() else defaultConfigurationJson
+
+            // Parse configuration
             val config = json.decodeFromString<HolonetConfiguration>(jsonString)
+
+            // Cache configuration
             cachedConfig = config
+
+            // If configuration does not exist, write default configuration
+            if (!configExists) writeConfiguration(config)
+
+            // Return configuration
             config
+        }
+    }
+
+    suspend fun writeConfiguration(configuration: HolonetConfiguration) {
+        withContext(Dispatchers.IO) {
+            val file = File(System.getProperty("user.home") + "/holonet/config.json")
+            file.parentFile.mkdirs()
+            file.writeText(json.encodeToString(configuration))
         }
     }
 }
@@ -32,6 +56,24 @@ private val testJson: String = """
             "priority": 1,
             "config": {
                 "url": "https://calendar.google.com/calendar/ical/2vtvf94piqaq2fkc3eju74rjjc%40group.calendar.google.com/private-7feea036760e905ce7e11ead218b804a/basic.ics"
+            }
+        }
+    }
+}
+""".trimIndent()
+
+private val defaultConfigurationJson: String = """
+{
+    "modules": {
+        "clock": {
+            "position": "top_left",
+            "priority": 0
+        },
+        "calendar": {
+            "position": "top_left",
+            "priority": 1,
+            "config": {
+                "url": "https://ics.calendarlabs.com/226/06a1cf11/UN_Holidays.ics"
             }
         }
     }
