90c56d1216437b0150108b16bec4e61eb23084d9
Mathias Jelshøj <mathias.j@shapegames.com>
2025-03-26 14:51:31 +0100

    Reorganize dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
         val desktopMain by getting
         
         commonMain.dependencies {
+            // Compose
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
@@ -36,15 +37,31 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            // Lifecycle and ViewModel
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // PF4J
             implementation(libs.pf4j)
+
+            // Serialization
+            implementation(libs.kotlinx.serialization.json)
+
+            // Koin
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+
+            // Ktor
+            implementation(project.dependencies.platform(libs.ktor.bom))
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.ktor.server.core)
+            implementation(libs.ktor.server.netty)
+
+            // Holonet Core
             implementation(libs.holonet.core)
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-            implementation("io.insert-koin:koin-core:4.0.2")
-            implementation("io.insert-koin:koin-compose:4.0.2")
-            implementation("io.ktor:ktor-client-core:3.1.0")
-            implementation("io.ktor:ktor-client-cio:3.1.0")
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
