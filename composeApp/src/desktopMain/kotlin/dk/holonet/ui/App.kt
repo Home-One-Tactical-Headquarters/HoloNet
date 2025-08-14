d1645330976b1760c62d00846181d27c038cbbbf
Mathias Jelshøj <mathias.j@shapegames.com>
2025-08-14 10:50:17 +0200

    Refactor module rendering logic for improved readability

--- composeApp/src/desktopMain/kotlin/dk/holonet/ui/App.kt
+++ composeApp/src/desktopMain/kotlin/dk/holonet/ui/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dk.holonet.components.BorderPane
+import dk.holonet.core.HoloNetModule
 import dk.holonet.core.Position
 import dk.holonet.theme.HoloNetTheme
 import org.koin.compose.viewmodel.koinViewModel
@@ -24,18 +25,17 @@ fun App(
                 is UiState.Loaded -> {
                     val modules = state.modules
                     BorderPane(
-                        topBar = modules.filter { it.position == Position.TOP_BAR }.map { { it.render() } },
-                        bottomBar = modules.filter { it.position == Position.BOTTOM_BAR }.map { { it.render() } },
-                        topLeft = modules.filter { it.position == Position.TOP_LEFT }.sortedBy { it.priority }
-                            .map { { it.render() } },
-                        topCenter = modules.filter { it.position == Position.TOP_CENTER }.map { { it.render() } },
-                        topRight = modules.filter { it.position == Position.TOP_RIGHT }.map { { it.render() } },
-                        upperThird = modules.filter { it.position == Position.UPPER_THIRD }.map { { it.render() } },
-                        middleCenter = modules.filter { it.position == Position.MIDDLE_THIRD }.map { { it.render() } },
-                        lowerThird = modules.filter { it.position == Position.LOWER_THIRD }.map { { it.render() } },
-                        bottomLeft = modules.filter { it.position == Position.BOTTOM_LEFT }.map { { it.render() } },
-                        bottomCenter = modules.filter { it.position == Position.BOTTOM_CENTER }.map { { it.render() } },
-                        bottomRight = modules.filter { it.position == Position.BOTTOM_RIGHT }.map { { it.render() } }
+                        topBar = modules.getModulesByPosition(Position.TOP_BAR),
+                        bottomBar = modules.getModulesByPosition(Position.BOTTOM_BAR),
+                        topLeft = modules.getModulesByPosition(Position.TOP_LEFT),
+                        topCenter = modules.getModulesByPosition(Position.TOP_CENTER),
+                        topRight = modules.getModulesByPosition(Position.TOP_RIGHT),
+                        upperThird = modules.getModulesByPosition(Position.UPPER_THIRD),
+                        middleCenter = modules.getModulesByPosition(Position.MIDDLE_THIRD),
+                        lowerThird = modules.getModulesByPosition(Position.LOWER_THIRD),
+                        bottomLeft = modules.getModulesByPosition(Position.BOTTOM_LEFT),
+                        bottomCenter = modules.getModulesByPosition(Position.BOTTOM_CENTER),
+                        bottomRight = modules.getModulesByPosition(Position.BOTTOM_RIGHT)
                     )
                 }
                 is UiState.Error -> {}
@@ -43,4 +43,8 @@ fun App(
 
         }
     }
+}
+
+private fun List<HoloNetModule>.getModulesByPosition(position: Position): List<@Composable () -> Unit> {
+    return this.filter { it.position == position }.sortedBy { it.priority }.map { { it.render() } }
 }
