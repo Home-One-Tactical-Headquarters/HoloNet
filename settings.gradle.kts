30abe303c5c72ac16013cd00faa4e8c844a216b7
Mathias Jelshøj <mathias.j@shapegames.com>
2025-08-25 11:33:39 +0200

    Add reckon plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,4 +30,17 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.ajoberstar.reckon.settings") version "0.19.2"
+}
+
+extensions.configure<org.ajoberstar.reckon.gradle.ReckonExtension> {
+    setDefaultInferredScope("patch")
+    stages("beta", "rc", "final")
+    setScopeCalc(calcScopeFromProp().or(calcScopeFromCommitMessages()))
+    setStageCalc(calcStageFromProp())
+}
+
+
+
 include(":composeApp")
